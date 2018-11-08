84e23f3b07e54ecd0f202b3d946e089ea52a19b9
BSDinis <baltasardinis@hotmail.com>
2018-11-08 02:28:37 +0000

    updated lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -13,6 +13,11 @@ def cacheL1_access_time(ways):
 def cacheL1_price(bytes):
     return bytes * 9 / (2**20)
 
+def cacheL2_access_time(ways):
+    return 7.5 + 2.5 * math.log2(ways);
+
+def cacheL2_price(bytes):
+    return bytes * 0.4 / (2**20)
 
 def ex231():
     print("Compulsory misses: the cache starts up empty, therefore, the data has to be loaded from memory. These are unavoidable misses")
@@ -77,6 +82,9 @@ def ex233():
             8: {1: 0.0305, 2: 0.0357, 4: 0.0027, 8: 0.0027}
             }
 
+    hit_time = { 16: {}, 8: {}}
+    miss_time = 150
+
     for bsize in {8, 16}:
         for ways in range(4):
             miss_r = miss_rate[bsize][2**ways]
@@ -85,7 +93,64 @@ def ex233():
             print(f"1024 - {bsize} - {2**ways} - Conflict - {conflict_fraction[bsize][2**ways] * miss_r}")
             print(f"1024 - {bsize} - {2**ways} - Total - {miss_r}")
 
+    print(f"AMAT for architecture w/ L1 cache::")
+    print(f"hit_time * (hit_rate) + miss_time * miss_rate")
+    print(f"hit_time = 1.5 + 0.5 * log_2(#ways)")
+    print(f"hit_rate = 1 - miss_rate")
+    print(f"miss_time = 150")
+    print(f"AMAT: (1.5 + 0.5 * log_2(#ways) ) * (1 - miss_rate) + 150 * miss_rate")
 
+
+    for bsize in {8, 16}:
+        for ways in range(4):
+            hit_time[bsize][2**ways] = cacheL1_access_time(2**ways)
+            miss_r = miss_rate[bsize][2**ways]
+            hit_r = hit_time[bsize][2**ways]
+            price = cacheL1_price(1024)
+            print(f"1024 - {bsize} - {2**ways}\t- Miss_Rate::{miss_r}\t- AMAT::{str(1.5 + 0.5 * ways * (1 -  miss_r) + 150 * miss_r)[:5]}\t- Price::{price}\t- Cost::{price * miss_r}")
+
+    print("Given that there are much fewer writes than reads, that we never write to the memory addresses we wish to read (the macroblocks) and the expensive operation is to compute the minimum SAD (which requires a lot of sequential accesses to the macroblocks) a write not allocate coupled with a write through makes sense. Also note that we only write the minimum SAD and the minimizer pair (i, j) and we don't read back from them nor do we write back right after their being written")
+    print()
+    print()
+    print("==And the winner is==")
+    bsize = 16
+    ways = 2
+    hit_time[bsize][2**ways] = cacheL1_access_time(2**ways)
+    miss_r = miss_rate[bsize][2**ways]
+    hit_r = hit_time[bsize][2**ways]
+    price = cacheL1_price(1024)
+    print(f" 1024 \n {bsize} \n {2**ways}\t\n Write Through & Write Not-Allocate\t\n Miss_Rate::{miss_r}\t\n AMAT::{str(1.5 + 0.5 * ways * (1 -  miss_r) + 150 * miss_r)[:5]}\t\n Price::{price}\t\n Cost::{price * miss_r}")
+
+                                                                                                                                                   
+def ex241():
+    n = 1
+    base_cost = cacheL1_price(1024) + frameMemoryCost()
+    cost = cacheL2_price(n) + base_cost
+    while cost <= 0.011:
+        old_cost = cost
+        n *= 2
+        cost = cacheL2_price(n * 2) + base_cost
+        
+
+    print(f"frame buffer memory: {frameMemoryCost()} €")
+    print(f"L1 cache price: {cacheL1_price(1024)} €")
+    print(f"L2 cache price: {cacheL2_price(n)} €")
+    print(f"price: {old_cost} €")
+    print(f"L1 size: {1024} B")
+    print(f"L2 size: {n} B")
+    print()
+
+    l1_bsize = 16
+
+    miss_rates =      		{ 16: .6117, 32: 0.3594, 64: 0.2985, 128: .2779}
+    compulsory_fraction = 	{ 16: .2444, 32: 0.2488, 64: 0.1498, 128: .2444}
+    capacity_fraction = 	{ 16: .7556, 32: 0.6528, 64: 0.8026, 128: .7556}
+    conflict_fraction = 	{ 16: .0000, 32: 0.0984, 64: 0.0476, 128: .2779}
+    for bsize in miss_rates:
+        print(f"L2 Cache:: Bsize = {bsize}\t-- Compulsory\t= {compulsory_fraction[bsize] * miss_rates[bsize]}")
+        print(f"L2 Cache:: Bsize = {bsize}\t-- Capacity\t= {capacity_fraction[bsize] * miss_rates[bsize]}")
+        print(f"L2 Cache:: Bsize = {bsize}\t-- Conflict\t= {conflict_fraction[bsize] * miss_rates[bsize]}")
+        print(f"L2 Cache:: Bsize = {bsize}\t-- Miss Rate\t= {miss_rates[bsize]}\t\t-- Penalty = {miss_rates[bsize] * bsize}")
 
 
 ex231()
@@ -93,4 +158,6 @@ print()
 ex232()
 print()
 ex233()
+print()
+ex241()
 print()
